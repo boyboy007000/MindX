00e90ce240178046504d43da5f05a23e76c6bc2e
boyboy007000 <boyboy007000@gmail.com>
2022-12-30 22:00:57 +0700

    linked_list

--- linked_list/source/node.cpp
+++ linked_list/source/node.cpp
@@ -51,3 +51,151 @@ void addLast(node * head, int value){
 }
 
 // them vao vi tri o giua
+
+void addMid(node * head, int value, int k){
+    node *p = getNode(value);
+    node *cusor = head;
+    node *temp = NULL;
+    if (head == NULL){
+        head = p;
+    }
+    else{
+        for (cusor; cusor->pNext && k > 0; cusor= cusor->pNext, k--)
+        temp = cusor->pNext; // *temp = cusor->pNext
+        cusor->pNext = p;
+        p->pNext = temp;
+    }
+}
+
+void delFirst(node * head){
+    node *p = head;
+    if (head == NULL){
+        return;
+    }
+    else{
+        head = head->pNext;
+        free(p);
+
+    }
+}
+
+void delLast(node * head){
+    node *cusor = head;
+    if (head == NULL){
+        return;
+    }
+    else{
+        for (cusor; cusor->pNext; cusor= cusor->pNext)
+        free(cusor);
+    }
+}
+
+void delMid(node * head, int k){
+    node *cusor = head;
+    node *temp = NULL;
+    if (head == NULL){
+        return;
+    }
+    else{
+        for (cusor; cusor->pNext && k > 1; cusor= cusor->pNext, k--)
+        temp = cusor->pNext;
+        cusor->pNext = temp->pNext;
+        free(temp);
+    }
+}
+
+void delValueK (node * head, int k){
+     node *cusor = head;
+    node *temp = NULL;
+    if (head == NULL){
+        return;
+    }
+    else{
+        for (cusor; cusor->pNext && cusor->pNext->value !=k; cusor= cusor->pNext)
+        temp = cusor->pNext;
+        cusor->pNext = temp->pNext;
+        free(temp);
+    }
+}
+
+// ap dung quick sort cho danh sach lien ket
+
+void slitList(node * head){
+    node *cusor = head;
+    node *odd = NULL;
+    node *even = NULL;
+    if (head == NULL){
+        return;
+    }
+    else{
+        for (cusor; cusor->pNext ; cusor= cusor->pNext){
+            if (cusor->value %2 == 0){
+                addFirst(even, cusor->value);
+            }
+            else{
+                addFirst(odd, cusor->value);
+            }
+        }
+    }
+}
+
+// de quy, tim kiem, sap xep, dslk
+
+int countNode(node * head){
+    node *cusor = head;
+    int count = 0;
+    if (head == NULL){
+        return;
+    }
+    else{
+        for (cusor; cusor->pNext ; cusor= cusor->pNext){
+            count++;
+        }
+    }
+    return count;
+}
+
+void addLast(node * head, node * current){
+    node * cusor = head;
+    if (head == NULL){
+        head = current;
+    }
+    else{
+        while (cusor)
+        {
+            cusor = cusor->pNext;
+        }
+        cusor->pNext = current;
+    }
+}
+
+void reverseList(node * head){
+    node * head2 = NULL;
+    node *cusor = head;
+    while (cusor){
+        node *preNode = NULL;
+        if (cusor->pNext->pNext =  NULL){
+            preNode = cusor;
+        }
+        if (cusor->pNext == NULL){
+            addLast(head2, cusor);
+            cusor = head;
+            preNode->pNext = NULL;
+        }
+    }
+}
+
+void reverseList(node * head){
+     node *pre = NULL;
+     node *cur = head;
+     node *next = NULL;
+     while (cur)
+     {
+        next = cur->pNext;
+        cur->pNext = pre;
+        pre = cur;
+        cur = next;
+     }
+     head = pre;
+     
+}
